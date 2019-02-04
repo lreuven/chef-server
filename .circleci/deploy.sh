4b273f6eaad6d1ce4396be0baf4d4f3de88ba4d4
shmuel <moly.witman@gmail.com>
2019-02-04 15:34:39 +0200

    deploy.sh chmod

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,7 +4,6 @@
 set -e
 
 function deploy () {
-    echo "In Deploy"
     git clone https://github.com/inPact/helm-live.git /tmp/helm-live
     #helm upgrade report-server /tmp/helm-live/azure/dev/services/reporting-server --set image.tag="${CIRCLE_BRANCH////_}-$(echo $CIRCLE_SHA1 | cut -c -7)"
     helm upgrade report-server "$live_path" --set image.tag="${CIRCLE_BRANCH////_}-$(echo $CIRCLE_SHA1 | cut -c -7)"
@@ -16,11 +15,13 @@ function deploy () {
 if [[ "$CIRCLE_TAG" =~ ^release-.*$ ]]; then
   echo "Not in use  !!!! - Found release tag. Deploying $SERVICES to $ACCOUNT in $REGIONS"
 elif [[ "$CIRCLE_BRANCH" == "master" ]]; then
-  echo "On master branch. Deploying $SERVICES to $ACCOUNT in $REGIONS"
+ echo "On $CIRCLE_BRANCH branch. with $live_path"
 elif [[ "$CIRCLE_BRANCH" == "azure-dev" ]]; then #git push origin HEAD:azure-dev
-   export live_path="/tmp/helm-live/azure/dev/services/reporting-server"
+    export live_path="/tmp/helm-live/azure/dev/services/reporting-server"
 elif [[ "$CIRCLE_BRANCH" == "azure-int-il" ]]; then
+    echo "On $CIRCLE_BRANCH branch. with $live_path"
 elif [[ "$CIRCLE_BRANCH" == "azure-stage-il" ]]; then
+    echo "On $CIRCLE_BRANCH branch. with $live_path"
 elif [[ "$CIRCLE_BRANCH" == "azure-prd-il" ]]; then
 else
   echo "Did not find release tag or master branch, so skipping deploy."
