09db7df8f5fb8b8c24d1eaca1e61035b0d92a42c
shmuel <moly.witman@gmail.com>
2019-02-05 09:44:42 +0200

    wow

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,8 +4,7 @@
 set -e
 
 function deploy () {
-    #git clone https://github.com/inPact/helm-live.git /tmp/helm-live
-    #helm upgrade report-server /tmp/helm-live/azure/dev/services/reporting-server --set image.tag="${CIRCLE_BRANCH////_}-$(echo $CIRCLE_SHA1 | cut -c -7)"
+    git clone https://github.com/inPact/helm-live.git /tmp/helm-live
     helm upgrade report-server "$live_path" --set image.tag="${CIRCLE_BRANCH////_}-$(echo $CIRCLE_SHA1 | cut -c -7)"
     echo "Helm $CIRCLE_BRANCH branch. with $live_path"
 }
@@ -17,9 +16,7 @@ if [[ "$CIRCLE_TAG" =~ ^release-.*$ ]]; then
 elif [[ "$CIRCLE_BRANCH" == "master" ]]; then
  echo "On $CIRCLE_BRANCH branch. with $live_path"
 elif [[ "$CIRCLE_BRANCH" == "azure-dev" ]]; then #git push origin HEAD:azure-dev
-    echo "On 11111111111111"
     export KUBECONFIG="/tmp/helm-live/azure/dev/kube_config"
-    echo "2222222 $KUBECONFIG"
     export live_path="/tmp/helm-live/azure/dev/services/reporting-server"
 elif [[ "$CIRCLE_BRANCH" == "azure-int-il" ]]; then
     echo "On $CIRCLE_BRANCH branch. with $live_path"
@@ -32,8 +29,4 @@ else
   exit 0
 fi
 
-# All commits will be from the machine user
-git config --global user.name "molywitman"
-git config --global user.email "moly.witman.gmail.com"
-#git config --global push.default simple
 deploy
