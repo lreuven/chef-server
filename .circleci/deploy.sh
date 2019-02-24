21714f73288a02ace46ce37a1006c52f56a54eb2
shmuel <moly.witman@gmail.com>
2019-02-24 16:45:24 +0200

    azure-il-int

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -47,6 +47,13 @@ elif [[ "$CIRCLE_BRANCH" == "azure-il-int" ]]; then #git push origin HEAD:azure-
     export namespace="int-il"
     export name=il-int-report-server
    echo "On $CIRCLE_BRANCH branch. with $live_path"
+elif [[ "$CIRCLE_BRANCH" == "azure-il-stg" ]]; then #git push origin HEAD:azure-il-int
+    export KUBECONFIG="/tmp/helm-live/azure/dev/aks/kube_config"  #TO DO check if need to change
+    export live_path="/tmp/helm-live/azure/reporting-server"
+    export values_file="/tmp/helm-live/azure/dev/il-stg/services/report-server/values.yaml"
+    export namespace="int-stg"
+    export name=il-stg-report-server
+   echo "On $CIRCLE_BRANCH branch. with $live_path"
 else
   echo "Did not find release tag or master branch, so skipping deploy."
   exit 0
